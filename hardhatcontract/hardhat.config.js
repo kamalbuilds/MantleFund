eb2a72e5811467589f989307b15ad09a76a9847f
legendarykamal <geniusamansingh@gmail.com>
2023-02-12 23:01:18 +0530

    hardhat project setup

---
+++ hardhatcontract/hardhat.config.js
@@ -0,0 +1,24 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'goerli',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
+      "mantle-testnet": {
+        url: "https://rpc.testnet.mantle.xyz/",
+        accounts: [process.env.PRIV_KEY] // Uses the private key from the .env file
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
