b748cc3131f39156298ba14d073226bb7068dfa3
Kamal Nayan <geniusamansingh@gmail.com>
2023-02-12 20:56:32 +0000

    feat wagmi connection added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,22 +10,65 @@ import './index.css';
 
 // wagmi 
 
-// import { configureChains, mainnet } from 'wagmi'
-// import { publicProvider } from 'wagmi/providers/public'
+import { WagmiConfig, createClient, configureChains, mainnet } from 'wagmi'
+ 
+import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { publicProvider } from 'wagmi/providers/public'
+ 
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+ 
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet],
+  [alchemyProvider({ apiKey: 'yourAlchemyApiKey' }), publicProvider()],
+)
+ 
+// Set up client
+const client = createClient({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
+  webSocketProvider,
+})
 
-// const { chains, provider, webSocketProvider } = configureChains(
-//  [mainnet],
-//  [publicProvider()],
-// )
+import {Connect } from "../src/pages/Connect";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
-    <Router>
-      <StateContextProvider>
-        <App />
-      </StateContextProvider>
-    </Router>
-  </ThirdwebProvider> 
+  <WagmiConfig client={client}>
+    <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+      <Router>
+        <StateContextProvider>
+          <Connect />
+          <App />
+        </StateContextProvider>
+      </Router>
+    </ThirdwebProvider> 
+  </WagmiConfig>
 )
