100232c2cccca973cb8d9d560f1fdc15adcb6eb6
kamalishere <kamaleinstein1@gmail.com>
2023-02-13 19:25:54 +0000

    adding arcana wallet

---
+++ client/src/pages/Connect.tsx
@@ -0,0 +1,32 @@
+import { useAccount, useConnect } from "wagmi";
+
+export default function Connect() {
+  const { connector, address, isConnected } = useAccount();
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
+
+  return (
+    <div className="main">
+      {isConnected && (
+        <div className="connected-msg">
+          Connected to {connector?.name} with address {address}
+        </div>
+      )}
+      {!isConnected &&
+        connectors.map((connector) => (
+          <button
+            className="connect-btn"
+            disabled={!connector.ready}
+            key={connector.id}
+            onClick={() => connect({ connector })}
+          >
+            Connect to {connector.name}
+            {isLoading &&
+              pendingConnector?.id === connector.id &&
+              " (connecting)"}
+          </button>
+        ))}
+      {error && <div>{error.message}</div>}
+    </div>
+  );
+}
